e94e1e735d538562589ac67da33a4438c71c9c36
Lukasz Matusek <Lukasz.Matusek@gmail.com>
2013-12-14 11:54:38 +0100

    Interface testowy

---
+++ Biblioteka/Biblioteka/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Biblioteka
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            int x = Form.ActiveForm.Width - 10;
+            int y = Form.ActiveForm.Height - 62;
+            Zakladki.Size = new Size(x,y);
+        }
+
+        
+
+       
+
+
+
+     
+
+
+    }
+}
